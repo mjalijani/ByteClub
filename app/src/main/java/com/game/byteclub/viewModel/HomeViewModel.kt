969c5f436c8c44b639853aa034e591b9bec3d94c
Mohammad javad <mj.alijani@gmail.com>
2023-03-12 14:47:17 +0330

    trying to make it better in syntax
    refactor codes

--- app/src/main/java/com/game/byteclub/viewModel/HomeViewModel.kt
+++ app/src/main/java/com/game/byteclub/viewModel/HomeViewModel.kt
@@ -12,19 +12,21 @@ import com.game.byteclub.view.adapter.GamesAdapter
 class HomeViewModel : ViewModel() {
 
     var dowrTxt = "Dowr"
-    private val adapter: GamesAdapter by lazy {
+    private val gamesAdapter: GamesAdapter by lazy {
         GamesAdapter(mutableListOf(
             Game("Dowr",R.id.dowrHomeFragment),
             Game("Spy Fall",R.id.dowrHomeFragment) //todo : change it later
         ))
     }
 
-    fun initRecyclerView(recyclerView: RecyclerView) {
-        recyclerView.adapter = adapter
-        val manager = LinearLayoutManager(recyclerView.context)
-        manager.orientation = LinearLayoutManager.VERTICAL
-        manager.reverseLayout = true
-        recyclerView.layoutManager = manager
+   fun initRecyclerView(recyclerView: RecyclerView) = recyclerView.apply {
+        adapter = gamesAdapter
+        val lm = LinearLayoutManager(recyclerView.context)
+        lm.apply {
+            orientation = LinearLayoutManager.VERTICAL
+            reverseLayout = true
+        }
+        layoutManager = lm
     }
 
 }
